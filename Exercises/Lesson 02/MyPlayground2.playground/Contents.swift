0b6215e33360ea2ef16a2ff318525f571bded2ec
slangton <sean@lokelyapp.com>
2015-05-11 13:46:36 -0400

    updates

---
+++ Exercises/Lesson 02/MyPlayground2.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+
+class Phone {
+    var name: String
+    
+    init(name: String) {
+    }
+}
